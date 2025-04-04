b75287f12513bca0ec6bda6f97e728e6aca97be3
mayank <mayanksingh90200>
2025-04-04 09:57:32 +0530

    midd

---
+++ middleware.js
@@ -0,0 +1,29 @@
+ const express=require('express');
+const app=express();
+const port=4000;
+//userdefined middle ware
+const adi=(req,res,next)=>{
+     const age=req.query.age
+    if(age!)
+    {
+        res.send("plz enter age")
+    }
+    if(age<=18)
+    {
+        res.send("sorry your age is under 18")
+    }
+    next();
+}
+
+
+app.get('/',(req,res)=>{
+    res.send('this is my home page')
+})
+
+app.get('/about',adi,(req,res)=>{
+    res.send("this is my about page")
+})
+
+app.listen(port,()=>{
+    console.log(`app is run at:${port}`)
+})
